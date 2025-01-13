d725840a683888312dc0341098b449a52f1cec47
ALI254746! <hojiakbarmuhammad77@gmail.com>
2025-01-13 15:48:31 +0500

    2-dars express localhostga so'rov yuborish

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 import express from "express";
-const app=express()
-const PORT=process.env.PORT ||4100
-app.listen(4100, () => console.log(`server is running ${PORT}`))
+import path, { dirname } from 'path';
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views' ,'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','about.html'));
+});
+
+const PORT = process.env.PORT || 4100;
+app.listen(PORT, () => console.log(`server ${PORT} portda ishlamoqda`));
