703acb223ef4995f4923650a9212dd6ddbee263b
ALI254746! <hojiakbarmuhammad77@gmail.com>
2025-01-13 16:25:39 +0500

    handlebarsdan foydalanish va qulayligi express bilan

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 import express from "express";
 import path, { dirname } from 'path';
 import { fileURLToPath } from "url";
+//handlebars devishogini imort qilib olamiz
+import {engine} from "express-handlebars";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
+//bu buyruqlar orqali handlebarsni joylab olamiz
+  app.engine('handlebars',engine())
+  app.set('view engine','handlebars')
+  app.set('views','./views')
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views' ,'index.html'));
+//biz handlebars divishogidan foydalansak code miz oson o'qiladi 
+app.get('/index', (req, res) => {
+   // res.sendFile(path.join(__dirname, 'views' ,'index.html'));
+   res.render('index')
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname,'views','about.html'));
+   // res.sendFile(path.join(__dirname,'views','about.html'));
+   res.render('about');
 });
 
 const PORT = process.env.PORT || 4100;
