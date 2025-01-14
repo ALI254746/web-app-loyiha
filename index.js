f1193f78586ac351cf94f2d611163da1ac5e6dd9
ALI254746! <hojiakbarmuhammad77@gmail.com>
2025-01-14 14:11:20 +0500

    mongodb va uni ulash dotenv parol xavsizligi

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import express from "express";
 import {create} from "express-handlebars";
+import mongoose, { trusted } from "mongoose";
+import * as dotenv from 'dotenv';
+
+
+
+//ROUTES
 import AuthRoutes from './routes/auth.js';
 import ProductRoutes from './routes/product.js';
 
 
+
+
+dotenv.config()
 
 const app = express();
 
@@ -22,12 +31,26 @@ const hbs=create({
 
  //fileni static qilib inde.css file ulashimiz mumkun footerga ham shunday qilib index.js file qo'shak boladi
  app.use(express.static('public'))
- 
+ //
+ app.use(express.json())
   app.use(AuthRoutes)
   app.use(ProductRoutes)
- 
-  
-
-
+ //bu pastdagi kod hozirgi versiyada ishlamaydi
+  // mongoose.connect(process.env.MONGO_URL,{
+  //   useNewUrlParser:true
+  // }, () => console.log("mongo db connected"))
+  // mongoose.set('strictQuery',false)
+  async function connectDB() {
+     try { 
+      await mongoose.connect(process.env.MONGO_URL, {
+       
+        }); 
+        console.log('MongoDB ga muvaffaqiyatli ulandi');
+       } catch (error) {
+         console.error('MongoDB ga ulanishda xatolik:', error); } } 
+         connectDB();
+// console.log(process.env.MONGO_URL)
 const PORT = process.env.PORT || 4100;
+
 app.listen(PORT, () => console.log(`server ${PORT} portda ishlamoqda`));
+  
